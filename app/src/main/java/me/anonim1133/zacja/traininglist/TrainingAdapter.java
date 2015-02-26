ab9660901599825f404b772f2a7fc84e04925130
anonim1133 <anonim1133@wp.pl>
2015-02-26 14:43:00 +0100

    Fixed displaying date on trainings list.

--- app/src/main/java/me/anonim1133/zacja/traininglist/TrainingAdapter.java
+++ app/src/main/java/me/anonim1133/zacja/traininglist/TrainingAdapter.java
@@ -2,12 +2,17 @@ package me.anonim1133.zacja.traininglist;
 
 import android.content.Context;
 import android.database.Cursor;
+import android.net.ParseException;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CursorAdapter;
 import android.widget.TextView;
 
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import me.anonim1133.zacja.R;
 
@@ -30,8 +35,24 @@ public class TrainingAdapter extends CursorAdapter {
 		TextView tv_points = (TextView)view.findViewById(R.id.training_points);
 
 		String txt_type = cursor.getString(cursor.getColumnIndexOrThrow("training_type"));
-		//ToDo: Wyciągnąć datę
-		String txt_date = cursor.getString(cursor.getColumnIndexOrThrow("gpx"));
+
+		String txt_date = "";
+		String dtStart = cursor.getString(cursor.getColumnIndexOrThrow("gpx"));
+		SimpleDateFormat  format = new SimpleDateFormat("yyyyMMdd'T'HHmmss");
+		try {
+			Date date = format.parse(dtStart);
+
+			SimpleDateFormat fmtOut = new SimpleDateFormat("dd-MM-yyyy HH:mm");
+
+			txt_date = fmtOut.format(date);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (java.text.ParseException e) {
+			e.printStackTrace();
+		}
+
+
 		String txt_stats = cursor.getString(cursor.getColumnIndexOrThrow("distance"));
 		//ToDo: Zliczać punkty
 		String txt_points = "0 punktów"; //cursor.getString(cursor.getColumnIndexOrThrow("points"));
