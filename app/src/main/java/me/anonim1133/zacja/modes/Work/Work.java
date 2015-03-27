f1cd8d1e2cc826598e4172121d2bba404a7ac24a
anonim1133 <anonim1133@wp.pl>
2015-03-27 22:18:26 +0100

    Added timer in work module

--- app/src/main/java/me/anonim1133/zacja/modes/Work/Work.java
+++ app/src/main/java/me/anonim1133/zacja/modes/Work/Work.java
@@ -2,27 +2,64 @@ package me.anonim1133.zacja.modes.Work;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import me.anonim1133.zacja.R;
 
 public class Work extends Fragment {
 	View rootView;
 
+	ProgressBar pb;
+	TextView timer;
+
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		rootView = inflater.inflate(R.layout.work_main, container, false);
-/*
-		rootView.findViewById(R.id.btn_ctf).setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View view) {
 
-			}
-		});
-*/
+		pb = (ProgressBar) rootView.findViewById(R.id.progressBarToday);
+		timer = (TextView) rootView.findViewById(R.id.tv_timer);
+
+		Animation an = new RotateAnimation(0.0f, 90.0f, 250f, 273f);
+		//an.setFillAfter(true);
+		pb.startAnimation(an);
+
+		pb.setMax(60);
+		pb.setProgress(0);
 
 		return rootView;
+	}
+
+	@Override
+	public void onResume() {
+		super.onResume();
+
+		CountDownTimer countDownTimer = new CountDownTimer(60 * pb.getMax() * 1000, 500) {
+			// 500 means, onTick function will be called at every 500 milliseconds
+
+			@Override
+			public void onTick(long leftTimeInMilliseconds) {
+				long seconds = leftTimeInMilliseconds / 1000;
+				int barVal= pb.getMax() - (int)seconds/60;
+
+				Log.d("VAL", String.valueOf(barVal));
+
+				pb.setProgress(barVal);
+				timer.setText(String.format("%02d", seconds/60) + ":" + String.format("%02d", seconds%60));
+
+			}
+			@Override
+			public void onFinish() {
+
+			}
+		}.start();
+
 	}
 }
